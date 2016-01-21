68a312cde7fb0252c746c3f64705177986df01b0
blackergr <blackergr@yahoo.com>
2016-01-21 13:00:46 -0600

    added doPost lines

--- UnicornPhoenix/src/UnicornPhoenix/Servlets/AddEmail.java
+++ UnicornPhoenix/src/UnicornPhoenix/Servlets/AddEmail.java
@@ -1,11 +1,16 @@
 package UnicornPhoenix.Servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import UnicornPhoenix.Database.Email;
+import UnicornPhoenix.Database.UnicornPhoenixDB;
+
 
 /**
  * Servlet implementation class AddEmail
@@ -45,6 +50,12 @@ public class AddEmail extends Master {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Email e = new Email();
+		e.setEmail(request.getParameter("email"));
+		e.setType(request.getParameter("type"));
+		
+		UnicornPhoenixDB db = new UnicornPhoenixDB();
+		db.addEmail(e);
 	}
 
 }
--- UnicornPhoenix/src/UnicornPhoenix/Servlets/AddMedHistory.java
+++ UnicornPhoenix/src/UnicornPhoenix/Servlets/AddMedHistory.java
@@ -1,11 +1,16 @@
 package UnicornPhoenix.Servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import UnicornPhoenix.Database.MedicalHistory;
+import UnicornPhoenix.Database.UnicornPhoenixDB;
+
 
 /**
  * Servlet implementation class AddMedHistory
@@ -27,8 +32,8 @@ public class AddMedHistory extends Master {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		body.append( "<form id=addMedHistory>");
-		body.append("<div class='form-group'><label for='allergies'>Any allergies?</label><input type='text' class='form-control' id='allergies' placeholder='type None for no allergies'></div>");
-		body.append("<div class='form-group'><label for='surgerys'>Any recent surgerys?</label><input type='text' class='form-control' id='surgery'placeholder='type None for no recent surgerys'></div>");
+		//body.append("<div class='form-group'><label for='allergies'>Any allergies?</label><input type='text' class='form-control' id='allergies' placeholder='type None for no allergies'></div>");
+		//body.append("<div class='form-group'><label for='surgerys'>Any recent surgerys?</label><input type='text' class='form-control' id='surgery'placeholder='type None for no recent surgerys'></div>");
 		body.append("<div class='form-inline'><label for='height'>Current Height</label><input type='text' class='form-control' id='height' placeholder='5 feet 6 inches'></div></br>");
 		body.append("<div class='form-inline'><label for='weight'>Current Weight</label><input type='text' class='form-control' id='weight' placeholder='140lbs'></div></br>");
 		body.append("<div class='form-inline'><lbel for='bloodtype'>Blood Type </label>"+
@@ -44,13 +49,22 @@ public class AddMedHistory extends Master {
 			    "</select></div>");
 		body.append("<div class='form-group'><button type='submit' class='btn btn-default'>Submit</button></div></form>");
 		
+		super.doGet(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// setting to database
+		MedicalHistory m = new MedicalHistory();
+		m.setBloodType(request.getParameter("bloodtype"));
+		m.setCurrentWeight(request.getParameter("currentweight"));
+		m.setCurrentHeight(request.getParameter("currentheight"));
+		
+		
+		UnicornPhoenixDB db = new UnicornPhoenixDB();
+		db.addMedicalHistory(m);
 	}
 
 }
