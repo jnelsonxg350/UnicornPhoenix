2f8eb3f02b30e69c2568a784cd77fe0809036957
blackergr <blackergr@yahoo.com>
2016-02-04 20:40:34 -0600

    looking at differences

---
+++ UnicornPhoenix/src/UnicornPhoenix/Servlets/DeleteVisit.java
@@ -0,0 +1,20 @@
+package UnicornPhoenix.Servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import UnicornPhoenix.Database.*;
+@WebServlet("/DeleteVisit")
+public class DeleteVisit extends Master {
+	private static final long serialVersionUID = 1L;
+    public DeleteVisit() {
+        super();
+    }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		UnicornPhoenixDB db = new UnicornPhoenixDB();
+		db.deleteVisit(request.getParameter("id"));
+	}
+}
