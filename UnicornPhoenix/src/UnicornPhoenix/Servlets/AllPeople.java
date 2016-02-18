eb9d31380b0ce01d23a0603518ccf94603a46f8e
tessa <student@MININT-95QZ402>
2016-02-17 20:20:08 -0600

    tessa

--- UnicornPhoenix/src/UnicornPhoenix/Servlets/AllPeople.java
+++ UnicornPhoenix/src/UnicornPhoenix/Servlets/AllPeople.java
@@ -19,7 +19,7 @@ public class AllPeople extends Master {
     	body.append("<h1 style='text-align:center;' class='textcolor'><span class='glyphicon glyphicon-user' aria-hidden='true'></span>&nbsp People</h1>");
 		
     	//start the table
-    	body.append("<table class='table'><caption><button type='button' class='btn btn-default btnAddPerson'>Add Person</button></caption><thead><tr><th></th><th class='changetext'>First Name</th><th class='changetext'>Last Name</th></tr></thead>");
+    	body.append("<table class='table'><caption><button type='button' class='btn btn-default btnAddPerson'>Add Person</button></caption><thead><tr><th></th><th class='changetext'>First Name</th><th class='changetext'>Last Name</th><th class='changetext'>Type</th></tr></thead>");
     	body.append("<tbody>");	
     	
     	////add the people
@@ -28,7 +28,7 @@ public class AllPeople extends Master {
     	for(int i = 0;i<people.size();i++)
     	{
     		Person p = people.get(i);
-        	body.append("<tr><td><button type='button' class='btn btn-primary btn-xs btnEditPerson' data-id='" + p.getPersonID() + "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> </button><button type='button' class='btn btn-primary btn-xs btnDeletePerson' data-id='" + p.getPersonID() + "'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></button>&nbsp<button type='button' class='btn btn-primary btn-xs btnPersonDetails' data-id='" + p.getPersonID() + "'> <span class='glyphicon glyphicon-info-sign' aria-hidden='true'></span> </button></td><td class='textcolor'>" + p.getFname() + "</td><td class='textcolor'>" + p.getLname() + "</td></tr>");
+        	body.append("<tr><td><button type='button' class='btn btn-primary btn-xs btnEditPerson' data-id='" + p.getPersonID() + "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span> </button><button type='button' class='btn btn-primary btn-xs btnDeletePerson' data-id='" + p.getPersonID() + "'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></button>&nbsp<button type='button' class='btn btn-primary btn-xs btnPersonDetails' data-id='" + p.getPersonID() + "'> <span class='glyphicon glyphicon-info-sign' aria-hidden='true'></span> </button></td><td class='textcolor'>" + p.getFname() + "</td><td class='textcolor'>" + p.getLname() + "</td><td class='textcolor'>" + p.getType() + "</td></tr>");
     	}
     	
     	//close the table
