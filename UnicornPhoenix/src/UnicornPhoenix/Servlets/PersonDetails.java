ad28b4b7849b50ca4dd13e417ac26280255a340e
tessa <student@MININT-95QZ402>
2016-02-19 18:07:25 -0600

    persondetails styes

--- UnicornPhoenix/src/UnicornPhoenix/Servlets/PersonDetails.java
+++ UnicornPhoenix/src/UnicornPhoenix/Servlets/PersonDetails.java
@@ -30,34 +30,37 @@ public class PersonDetails extends Master {
 		
 		//Print details
 		body = new StringBuilder();
-		body.append("<h4>First Name: " + p.getFname()+"</br></br>");
-		body.append("Middle Name: "+p.getMname()+"</br></br>");
-		body.append("Last Name: "+p.getLname()+"</br></br>");
-		body.append("Date of Birth:"+p.getDOB()+"</br></br>");
-		body.append("SSN: "+p.getSSN()+"</br></br>");
+		body.append("<h1 style='text-align:center;' class='titlecolor'>Personal information</h1>");
+		body.append("<div class='row'><div class='col-md-6'>");
+		body.append("<h4><span class='titlecolortoo'>First Name:</span><span class='textcolor'>&nbsp&nbsp " + p.getFname()+"</span></br></br>");
+		body.append("<span class='titlecolortoo'>Middle Name:</span><span class='textcolor'>&nbsp&nbsp "+p.getMname()+"</span></br></br>");
+		body.append("<span class='titlecolortoo'>Last Name:</span><span class='textcolor'>&nbsp&nbsp "+p.getLname()+"</span></br></br>");
+		body.append("<span class='titlecolortoo'>Date of Birth:</span><span class='textcolor'>&nbsp&nbsp"+p.getDOB()+"</span></br></br>");
+		body.append("<span class='titlecolortoo'>SSN:</span><span class='textcolor'>&nbsp&nbsp&nbsp "+p.getSSN()+"</span></br></br>");
 		if (p.getGender().toLowerCase().equals("f")){
-			body.append("Gender: Female </br></br>");
+			body.append("<span class='titlecolortoo'>Gender:</span><span class='textcolor'>&nbsp&nbsp  Female </span></br></br>");
 		}
 		else if (p.getGender().toLowerCase().equals("m")){
-			body.append("Gender: Male </br></br>");
+			body.append("<span class='titlecolortoo'>Gender:</span><span class='textcolor'>&nbsp&nbsp Male</span> </br></br>");
 		}
 		else if (p.getGender().toLowerCase().equals("o")){
-			body.append("Gender: Other </br></br>");
+			body.append("<span class='titlecolortoo'>Gender:</span><span class='textcolor'>&nbsp&nbsp Other</span> </br></br>");
 		}
 		if (p.getType().toLowerCase().equals("doctor")){
-			body.append("Person is a: Doctor </br></br>");
+			body.append("<span class='titlecolortoo'>Person is a:</span><span class='textcolor'>&nbsp&nbsp Doctor</span> </br></br>");
 		}
 	 if(p.getType().toLowerCase().equals("patient")){
-			body.append("Person is a: Patient </br></br>");
+			body.append("<span class='titlecolortoo'>Person is a:</span><span class='textcolor'>&nbsp&nbsp Patient </span></br></br>");
 		}
 	if (p.getGender().toLowerCase().equals("provider")){
-			body.append("Person is a: Provider </br></br>");
+			body.append("<span class='titlecolortoo'>Person is a:</span><span class='textcolor'>&nbsp&nbsp Provider</span> </br></br>");
 		}
 	
 		//Email
+	body.append("<h3 class='titlecolor'>Email:</h3>");
 		for(int i = 0;i<Emails.size();i++)
 		{
-		body.append("Email: "+Emails.get(i).getEmail()+"</br>");
+		body.append("<h4 class='textcolor'>Email: "+Emails.get(i).getEmail()+"</br>");
 		if (Emails.get(i).getType().toLowerCase().equals("personal")){
 			body.append("Email is: Personal ");
 		}
@@ -67,46 +70,47 @@ public class PersonDetails extends Master {
 		if(Emails.get(i).getType().toLowerCase().equals("other")){
 			body.append("Email is: Other ");
 		}
-		body.append("<button type='button' class='btn btn-default btn-xs btnUpdateEmail' data-emailid='" + Emails.get(i).getEmailID() + "'>Edit Email</button>&nbsp");
-		body.append("<button type='button' class='btn btn-primary btn-xs btnDeleteEmail' data-emailid='" + Emails.get(i).getEmailID() + "'>Delete</button></br></br>");
+		body.append("</h4><button type='button' class='btn btn-default btn-xs btnUpdateEmail' data-emailid='" + Emails.get(i).getEmailID() + "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span></button>&nbsp");
+		body.append("<button type='button' class='btn btn-primary btn-xs btnDeleteEmail' data-emailid='" + Emails.get(i).getEmailID() + "'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></button></br></br>");
 		}
 		
+		
 		//add email button 
-		body.append("<button type='button' class='btn btn-default btn-xs' id='addEmail' data-id='" + p.getPersonID() + "'>Add Email</button>");
-			
-		body.append("<h3>Allergies<h3>");
+		body.append("<button type='button' class='btn btn-default btn-xs' id='addEmail' data-id='" + p.getPersonID() + "'><span class='glyphicon glyphicon-plus' aria-hidden='true'></span>&nbsp Email</button></div>");
+			body.append("<div class='row'><div class='col-md-6'>");
+		body.append("<h3 class='titlecolor'>Allergies</h3><h4 class='textcolor'>");
 		for(int i = 0;i<allergies.size();i++)
 		{
-			body.append(allergies.get(i).getName() + "<br/>");
-			body.append("<button type='button' class='btn btn-default btn-xs btnUpdateAllergy' data-allergyid='" + allergies.get(i).getAllergyID() + "'>Edit Allergy</button>&nbsp");
-			body.append("<button type='button' class='btn btn-primary btn-xs btnDeleteAllergy' data-allergyid='" + allergies.get(i).getAllergyID() + "'>Delete</button></br></br>");
+			body.append(allergies.get(i).getName()+"&nbsp&nbsp");
+			body.append("<button type='button' class='btn btn-default btn-xs btnUpdateAllergy' data-allergyid='" + allergies.get(i).getAllergyID() + "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span></button>&nbsp");
+			body.append("<button type='button' class='btn btn-primary btn-xs btnDeleteAllergy' data-allergyid='" + allergies.get(i).getAllergyID() + "'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></button></br></br>");
 		
 		}
 		//button wont work
-		body.append("<button type='button' class='btn btn-default btn-xs' id='addAllergy' data-id='" + p.getPersonID() + "'>Add Allergy</button>");
+		body.append("<button type='button' class='btn btn-default btn-xs' id='addAllergy' data-id='" + p.getPersonID() + "'><span class='glyphicon glyphicon-plus' aria-hidden='true'></span>&nbsp Allergy</button>");
 		
 		//Medical History info update and delete button
-		body.append("<h3>Medical History<h3>");
+		body.append("<h3 class='titlecolor'>Medical History</h3><h4>");
 		for(int i = 0;i<MedicalHistorys.size();i++)
 		{
-			body.append(MedicalHistorys.get(i).getBloodType() + "  BloodType" + "   " + 
-					MedicalHistorys.get(i).getCurrentWeight() + "  Weight" + "   " +  
-					MedicalHistorys.get(i).getCurrentHeight() + "  Height" + "   ");
-			body.append("<button type='button' class='btn btn-default btn-xs btnUpdateMedicalHistory' data-medicalhistoryid='" + MedicalHistorys.get(i).getMedicalHistoryID() + "'>Edit Medical History</button>&nbsp");
-			body.append("<button type='button' class='btn btn-primary btn-xs btnDeleteMedicalHistory' data-medicalhistoryid='" + MedicalHistorys.get(i).getMedicalHistoryID() + "'>Delete</button></br></br>");
+			body.append("<span class='textcolor'>Blood Type: "+MedicalHistorys.get(i).getBloodType() + ", &nbsp" + 
+					"Weight: "+MedicalHistorys.get(i).getCurrentWeight()+ ", &nbsp" +  
+					"Height: "+MedicalHistorys.get(i).getCurrentHeight() + "</span> ");
+			body.append("<button type='button' class='btn btn-default btn-xs btnUpdateMedicalHistory' data-medicalhistoryid='" + MedicalHistorys.get(i).getMedicalHistoryID() + "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span></button>&nbsp");
+			body.append("<button type='button' class='btn btn-primary btn-xs btnDeleteMedicalHistory' data-medicalhistoryid='" + MedicalHistorys.get(i).getMedicalHistoryID() + "'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></button></br></br>");
 		}
 		
 		//add medical history button 
-		body.append("<button type='button' class='btn btn-default btn-xs' id='addMedHistory' data-id='" + p.getPersonID() + "'>Add Medical History</button>");
+		body.append("<button type='button' class='btn btn-default btn-xs' id='addMedHistory' data-id='" + p.getPersonID() + "'><span class='glyphicon glyphicon-plus' aria-hidden='true'></span>&nbsp Medical History</button>");
 		
 		//Visit
-		body.append("<h3>Visit<h3>");
+		body.append("<h3 class='titlecolor'>Visit</h3><h4 class='textcolor'>");
 		for(int i = 0;i<Visits.size();i++)
 		{
-			body.append("Date of Visit:"+Visits.get(i).getDateOfVisit()+"</br></br>");
-			body.append(Visits.get(i).getBloodPressure() + "  BloodPressure" + "   " + 
-					Visits.get(i).getWeight() + "  Weight" + "   " +  
-					Visits.get(i).getHeight() + "  Height" + "   "+"</br>");
+			body.append("Date of Visit:  "+Visits.get(i).getDateOfVisit()+"</br>");
+			body.append("Blood Pressure: " +Visits.get(i).getBloodPressure() +  ", &nbsp" + 
+					"Weight: " +Visits.get(i).getWeight() +  ", &nbsp" +  
+					"Height: " +Visits.get(i).getHeight() +"</br>");
 			body.append("Visit: "+Visits.get(i).getType()+"</br>");
 			if (Visits.get(i).getType().toLowerCase().equals("Walk-In")){
 			}
@@ -114,13 +118,13 @@ public class PersonDetails extends Master {
 			}
 			if(Visits.get(i).getType().toLowerCase().equals("Emergency")){
 			}
-			body.append("Result: "+Visits.get(i).getResult()+"</br>");
-			body.append("<button type='button' class='btn btn-default btn-xs btnUpdateVisit' data-visitid='" + Visits.get(i).getVisitID() + "'>Edit Visit</button>&nbsp");
-			body.append("<button type='button' class='btn btn-primary btn-xs btnDeleteVisit' data-visitid='" + Visits.get(i).getVisitID() + "'>Delete</button></br></br>");
+			body.append("Result: "+Visits.get(i).getResult()+"</br></br>");
+			body.append("<button type='button' class='btn btn-default btn-xs btnUpdateVisit' data-visitid='" + Visits.get(i).getVisitID() + "'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span></button>&nbsp");
+			body.append("<button type='button' class='btn btn-primary btn-xs btnDeleteVisit' data-visitid='" + Visits.get(i).getVisitID() + "'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span></button></br></br>");
 		}
 		
 		//add visit button 
-		body.append("<button type='button' class='btn btn-default btn-xs' id='addVisit' data-id='" + p.getPersonID() + "'>Add Visit</button>");
+		body.append("<button type='button' class='btn btn-default btn-xs' id='addVisit' data-id='" + p.getPersonID() + "'><span class='glyphicon glyphicon-plus' aria-hidden='true'></span>&nbsp Visit</button>");
 		
 		super.doGet(request, response);
 	}
